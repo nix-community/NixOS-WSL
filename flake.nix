fb9f1d8894046ff4aa2e30111293130f4e2bffa1
Vincent Haupert <mail@vincent-haupert.de>
2021-02-20 10:37:28 +0100

    flake.nix: add nixpkgs-fmt check

--- flake.nix
+++ flake.nix
@@ -1,22 +1,36 @@
-
 {
   description = "NixOS WSL";
 
- inputs = {
-    nixpkgs.url = "nixpkgs/nixos-20.09";
-  };
-  
-  outputs = { nixpkgs, ... }@inputs: {
-    nixosConfigurations = {
+  inputs.nixpkgs.url = "nixpkgs/nixos-20.09";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-      mysystem = inputs.nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    {
+      nixosConfigurations.mysystem = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           (import ./configuration.nix)
           (import ./build-tarball.nix)
         ];
-        specialArgs = { inherit (inputs) nixpkgs; };
+        specialArgs = { inherit nixpkgs; };
       };
-    };
-  };
+    } //
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        checks.check-format = pkgs.runCommand "check-format"
+          {
+            buildInputs = with pkgs; [ nixpkgs-fmt ];
+          } ''
+          nixpkgs-fmt --check ${./.}
+          mkdir $out # success
+        '';
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ nixpkgs-fmt ];
+        };
+      }
+    );
 }
