91b58a9f6c1664a9c34d5cc6201812d8b6cd0a99
Vincent Haupert <mail@vincent-haupert.de>
2021-02-20 10:46:36 +0100

    Run `nixpkgs-fmt .`

--- build-tarball.nix
+++ build-tarball.nix
@@ -1,15 +1,14 @@
 { config, pkgs, lib, nixpkgs ? <nixpkgs>, ... }:
 
 with lib;
-
 let
-   pkgs2storeContents = l : map (x: { object = x; symlink = "none"; }) l;
+  pkgs2storeContents = l: map (x: { object = x; symlink = "none"; }) l;
 
-   nixpkgs = lib.cleanSource pkgs.path;
+  nixpkgs = lib.cleanSource pkgs.path;
 
-   channelSources = pkgs.runCommand "nixos-${config.system.nixos.version}"
-     { preferLocalBuild = true; }
-     ''
+  channelSources = pkgs.runCommand "nixos-${config.system.nixos.version}"
+    { preferLocalBuild = true; }
+    ''
       mkdir -p $out
       cp -prd ${nixpkgs.outPath} $out/nixos
       chmod -R u+w $out/nixos
@@ -21,43 +20,44 @@ let
       echo ${config.system.nixos.versionSuffix} | sed -e s/pre// > $out/nixos/svn-revision
     '';
 
-   preparer = pkgs.writeShellScriptBin "wsl-prepare" ''
-     set -e
+  preparer = pkgs.writeShellScriptBin "wsl-prepare" ''
+    set -e
 
-     mkdir -m 0755 ./bin ./etc
-     mkdir -m 1777 ./tmp
+    mkdir -m 0755 ./bin ./etc
+    mkdir -m 1777 ./tmp
 
-     # WSL requires a /bin/sh - only temporary, NixOS's activate will overwrite
-     ln -s ${pkgs.stdenv.shell} ./bin/sh
+    # WSL requires a /bin/sh - only temporary, NixOS's activate will overwrite
+    ln -s ${pkgs.stdenv.shell} ./bin/sh
 
-     # WSL also requires a /bin/mount, otherwise the host fs isn't accessible
-     ln -s /nix/var/nix/profiles/system/sw/bin/mount ./bin/mount
+    # WSL also requires a /bin/mount, otherwise the host fs isn't accessible
+    ln -s /nix/var/nix/profiles/system/sw/bin/mount ./bin/mount
 
-     # Set system profile
-     system=${config.system.build.toplevel}
-     ./$system/sw/bin/nix-store --store `pwd` --load-db < ./nix-path-registration
-     rm ./nix-path-registration
-     ./$system/sw/bin/nix-env --store `pwd` -p ./nix/var/nix/profiles/system --set $system
+    # Set system profile
+    system=${config.system.build.toplevel}
+    ./$system/sw/bin/nix-store --store `pwd` --load-db < ./nix-path-registration
+    rm ./nix-path-registration
+    ./$system/sw/bin/nix-env --store `pwd` -p ./nix/var/nix/profiles/system --set $system
 
-     # Set channel
-     mkdir -p ./nix/var/nix/profiles/per-user/root
-     ./$system/sw/bin/nix-env --store `pwd` -p ./nix/var/nix/profiles/per-user/root/channels --set ${channelSources}
-     mkdir -m 0700 -p ./root/.nix-defexpr
-     ln -s /nix/var/nix/profiles/per-user/root/channels ./root/.nix-defexpr/channels
+    # Set channel
+    mkdir -p ./nix/var/nix/profiles/per-user/root
+    ./$system/sw/bin/nix-env --store `pwd` -p ./nix/var/nix/profiles/per-user/root/channels --set ${channelSources}
+    mkdir -m 0700 -p ./root/.nix-defexpr
+    ln -s /nix/var/nix/profiles/per-user/root/channels ./root/.nix-defexpr/channels
 
-     # It's now a NixOS!
-     touch ./etc/NIXOS
+    # It's now a NixOS!
+    touch ./etc/NIXOS
 
-     # Copy the system configuration
-     mkdir -p ./etc/nixos
-     cp ${./configuration.nix} ./etc/nixos/configuration.nix
-     cp ${./syschdemd.nix} ./etc/nixos/syschdemd.nix
-     cp ${./syschdemd.sh} ./etc/nixos/syschdemd.sh
-   '';
+    # Copy the system configuration
+    mkdir -p ./etc/nixos
+    cp ${./configuration.nix} ./etc/nixos/configuration.nix
+    cp ${./syschdemd.nix} ./etc/nixos/syschdemd.nix
+    cp ${./syschdemd.sh} ./etc/nixos/syschdemd.sh
+  '';
 in
-{  system.build.tarball = pkgs.callPackage "${nixpkgs}/nixos/lib/make-system-tarball.nix" {
+{
+  system.build.tarball = pkgs.callPackage "${nixpkgs}/nixos/lib/make-system-tarball.nix" {
     # No contents, structure will be added by prepare script
-    contents = [];
+    contents = [ ];
 
     storeContents = pkgs2storeContents [
       config.system.build.toplevel
