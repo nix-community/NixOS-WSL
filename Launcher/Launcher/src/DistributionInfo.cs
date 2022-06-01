6c85815dd8905965fca9536a4af6695439ad6687
nzbr <mail@nzbr.de>
2022-06-01 08:36:10 +0200

    Set correct distribution name

--- Launcher/Launcher/src/DistributionInfo.cs
+++ Launcher/Launcher/src/DistributionInfo.cs
@@ -2,6 +2,6 @@
 
 internal static class DistributionInfo {
     public const string DisplayName = "NixOS";
-    public const string Name = "NixOS-WSL"; // TODO: Change to NixOS
+    public const string Name = "NixOS"; // TODO: Change to NixOS
     public const string WindowTitle = "NixOS";
 }
