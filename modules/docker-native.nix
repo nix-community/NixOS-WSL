d820aadc95251acc49f37f1f62b23ad06af4c3c5
Sandro <sandro.jaeckel@gmail.com>
2022-06-15 16:59:22 +0200

    Don't add the default user to docker group by default (#105)
    * Don't add the default user to docker group by default
    
    Closes #101
    
    * Make default depend on security.sudo.wheelNeedsPassword

--- modules/docker-native.nix
+++ modules/docker-native.nix
@@ -3,6 +3,16 @@ with builtins; with lib; {
 
   options.wsl.docker-native = with types; {
     enable = mkEnableOption "Native Docker integration in NixOS.";
+
+    addToDockerGroup = mkOption {
+      type = bool;
+      default = config.security.sudo.wheelNeedsPassword;
+      description = ''
+        Wether to add the default user to the docker group.
+
+        This is not recommend to be enabled because it essentially permits unauthenticated root access.
+      '';
+    };
   };
 
   config =
@@ -23,9 +33,8 @@ with builtins; with lib; {
 
       virtualisation.docker.enable = true;
 
-      users.groups.docker.members = [
+      users.groups.docker.members = lib.mkIf cfg.addToDockerGroup [
         config.wsl.defaultUser
       ];
     };
-
 }
