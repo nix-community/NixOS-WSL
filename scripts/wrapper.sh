6ab9a9ea7d9979c9db136675aba2eddcb91badf7
nzbr <mail@nzbr.de>
2022-11-09 11:05:36 +0100

    make check happy

--- scripts/wrapper.sh
+++ scripts/wrapper.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 if grep -q binfmt_misc /proc/filesystems; then
-    mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
+  mount -t binfmt_misc binfmt_misc /proc/sys/fs/binfmt_misc
 fi
 
 exec systemd
