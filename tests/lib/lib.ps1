31d07f2b84a72738671737485b9e7961c161619c
nzbr <mail@nzbr.de>
2022-11-25 07:14:27 +0100

    Add lib implementation for Windows

--- tests/lib/lib.ps1
+++ tests/lib/lib.ps1
@@ -1,3 +1,4 @@
+# Implementation-independent base class
 class Distro {
   [string]$id
 
@@ -7,6 +8,19 @@ class Distro {
 
   [string]GetPath([string]$path) {
     throw "Not implemented"
+  }
+
+  [string]FindTarball() {
+    # Check if a fresh tarball exists in result, otherwise try one in the current directory
+    $tarball = "./result/tarball/nixos-wsl-installer.tar.gz"
+    if (!(Test-Path $tarball)) {
+      $tarball = "./nixos-wsl-installer.tar.gz"
+      if (!(Test-Path $tarball)) {
+        throw "Could not find the installer tarball! Run nix build first, or place one in the current directory."
+      }
+    }
+    Write-Host "Using tarball: $tarball"
+    return $tarball
   }
 
   [void]InstallConfig([string]$path) {
@@ -36,25 +50,17 @@ class Distro {
   }
 }
 
+# Emulates a WSL distro using a Docker container (for Linux hosts)
 class DockerDistro : Distro {
   static [string]$hostMount = "/mnt/c"
   static [string]$imageName = "local:nixos-wsl"
 
   static [bool]$imageCreated = $false
-  [string]$id
 
   DockerDistro() {
-    if (!([DockerDistro]::imageCreated)) {
-      # Check if a fresh tarball exists in result, otherwise try one in the current directory
-      $tarball = "./result/tarball/nixos-wsl-installer.tar.gz"
-      if (!(Test-Path $tarball)) {
-        $tarball = "./nixos-wsl-installer.tar.gz"
-        if (!(Test-Path $tarball)) {
-          throw "Could not find the installer tarball! Run nix build first, or place one in the current directory."
-        }
-      }
-      Write-Host "Using tarball: $tarball"
+    $tarball = $this.FindTarball()
 
+    if (!([DockerDistro]::imageCreated)) {
       # Build docker image from the installer tarball
       $tmpdir = $(mktemp -d)
       Copy-Item $PSScriptRoot/Dockerfile $tmpdir
@@ -98,9 +104,54 @@ class DockerDistro : Distro {
   }
 }
 
+# A real WSL distro (for Windows hosts)
+class WslDistro : Distro {
+  [string]$tempdir
+
+  WslDistro() {
+    $tarball = $this.FindTarball()
+
+    $this.id = [guid]::NewGuid().ToString()
+    $this.tempdir = Join-Path $([System.IO.Path]::GetTempPath()) $this.id
+    New-Item -ItemType Directory $this.tempdir
+
+    & wsl.exe --import $this.id $this.tempdir $tarball --version 2 | Write-Host
+    if ($LASTEXITCODE -ne 0) {
+      throw "Failed to import distro"
+    }
+    & wsl.exe --list | Should -Contain $this.id
+  }
+
+  [Array]Launch([string]$command) {
+    $result = @()
+    & wsl.exe (@("-d", "$($this.id)") + $command.Split()) | Tee-Object -Variable result | Write-Host
+    return $result
+  }
+
+  [string]GetPath([string]$path) {
+    return $this.Launch("wslpath $($path -replace "\\", "/")") | Select-Object -Last 1
+  }
+
+  [void]Shutdown() {
+    & wsl.exe -t $this.id
+    if ($LASTEXITCODE -ne 0) {
+      throw "Failed to stop distro"
+    }
+  }
+
+  [void]Uninstall() {
+    & wsl.exe --unregister $this.id | Write-Host
+    if ($LASTEXITCODE -ne 0) {
+      throw "Failed to unregister distro"
+    }
+    Remove-Item $this.tempdir -Recurse -Force
+  }
+}
+
+# Auto-select the implementation to use
 function Install-Distro() {
   if ($IsWindows) {
-
+    return [WslDistro]::new()
   }
   else {
     return [DockerDistro]::new()
