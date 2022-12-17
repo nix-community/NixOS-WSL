8d295219a6dd48fdd6d45b2ccec1467986911fa8
nzbr <mail@nzbr.de>
2022-12-17 14:18:47 +0100

    backport nixos-wsl-version

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
   };
 
   outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    with nixpkgs.lib;
     {
 
       nixosModules.wsl = {
@@ -21,8 +22,13 @@
           ./modules/docker-native.nix
           ./modules/installer.nix
           ./modules/interop.nix
+          ./modules/version.nix
           ./modules/wsl-conf.nix
           ./modules/wsl-distro.nix
+
+          ({ ... }: {
+            wsl.version.rev = mkIf (self ? rev) self.rev;
+          })
         ];
       };
 
