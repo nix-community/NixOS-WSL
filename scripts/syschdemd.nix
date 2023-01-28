14273c185311868e8525a0af3b5c93503e0f53e8
nzbr <mail@nzbr.de>
2023-01-28 07:04:21 +0100

    small improvements to tests

--- scripts/syschdemd.nix
+++ scripts/syschdemd.nix
@@ -10,7 +10,7 @@
 , which
 , defaultUser
 , automountPath
-,
+, ...
 }:
 let
   mkWrappedScript =
--- tests/systemd-services.Tests.ps1
+++ tests/systemd-services.Tests.ps1
@@ -8,12 +8,12 @@ Describe "Systemd Services" {
   }
 
   It "should boot" {
-    $distro.Launch(@("true"))
+    $distro.Launch("true")
     $LASTEXITCODE | Should -Be 0
   }
 
   It "should not have any failed unit" {
-    $output = $distro.Launch(@("sudo", "systemctl", "list-units", "--failed")) | Remove-Escapes
+    $output = $distro.Launch("sudo systemctl list-units --failed") | Remove-Escapes
     $output | Where-Object { $_.trim() -ne "" } | Select-Object -Last 1 | Should -BeExactly "0 loaded units listed."
     $LASTEXITCODE | Should -Be 0
   }
