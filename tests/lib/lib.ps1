589f87fff5a8abe6fef3db8aa0d462657645d98c
nzbr <mail@nzbr.de>
2023-01-29 08:01:13 +0100

    Remove unnecessary Split-String function

--- tests/lib/lib.ps1
+++ tests/lib/lib.ps1
@@ -12,32 +12,6 @@ function Remove-Escapes {
   }
 }
 
-function Split-String {
-  [CmdletBinding()]
-  param (
-    [Parameter(Mandatory = $true)]
-    [string]$InputString
-  )
-
-  # use a regular expression to match quoted parts of the input string
-  $regex = '(?<=^|\s)("[^"]*"|''[^'']*''|[^\s]+)'
-  $matches = [regex]::matches($InputString, $regex)
-
-  # create an array to store the split parts of the string
-  $splitString = @()
-
-  # loop through the matches and add each part to the array
-  foreach ($match in $matches) {
-    # remove the quotes from the start and end of the match
-    $part = $match.Value.Trim("'").Trim('"')
-
-    $splitString += $part
-  }
-
-  # return the split parts of the string
-  return $splitString
-}
-
 # Implementation-independent base class
 class Distro {
   [string]$id
