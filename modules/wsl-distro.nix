562dd7206b53196d51592d62a880a4c2dd832a9e
K900 <me@0upti.me>
2023-01-30 08:58:48 +0300

    fix: ensure /nix/nixos-wsl exists before linking entrypoint

--- modules/wsl-distro.nix
+++ modules/wsl-distro.nix
@@ -105,6 +105,7 @@ with lib; {
               ln -sf ${pkgs.util-linux}/bin/mount /bin/mount
             '';
             update-entrypoint.text = ''
+              mkdir -p /nix/nixos-wsl
               ln -sf ${config.users.users.root.shell} /nix/nixos-wsl/entrypoint
             '';
           };
