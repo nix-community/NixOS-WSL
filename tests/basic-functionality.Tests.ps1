0762c9b399b7e507cf116a8f31e2e13fe1e82a0b
nzbr <mail@nzbr.de>
2023-07-14 09:53:38 +0200

    Add additional tests

--- tests/basic-functionality.Tests.ps1
+++ tests/basic-functionality.Tests.ps1
@@ -7,13 +7,18 @@ Describe "Basic Functionality" {
     $distro = Install-Distro
   }
 
-  It "is possible to run a command through the installer" {
+  It "is possible to run a command initially" {
     $distro.Launch("nixos-version")
     $LASTEXITCODE | Should -Be 0
   }
 
   It "is possible to run a second command" {
     $distro.Launch("true")
+    $LASTEXITCODE | Should -Be 0
+  }
+
+  It "is possible to run a command through sudo" {
+    $distro.Launch("sudo whoami") | Select-Object -Last 1 | Should -BeExactly "root"
     $LASTEXITCODE | Should -Be 0
   }
 
@@ -24,7 +29,28 @@ Describe "Basic Functionality" {
   }
 
   It "is possible to use nixos-rebuild" {
+    $temp = New-TemporaryFile
+    @"
+      { pkgs, config, ... }:
+      {
+      imports = [ ./base.nix ];
+
+      environment.systemPackages = with pkgs; [ hello ];
+      }
+"@ >  $temp
+    $distro.InstallConfig($temp)
+    Remove-Item $temp
     $distro.Launch("sudo nixos-rebuild switch")
+    $LASTEXITCODE | Should -Be 0
+  }
+
+  It "should have created a new generation" {
+    $distro.Launch("test -L /nix/var/nix/profiles/system-2-link")
+    $LASTEXITCODE | Should -Be 0
+  }
+
+  It "should be possible to run a command installed with the rebuild" {
+    $distro.Launch("hello")
     $LASTEXITCODE | Should -Be 0
   }
 
