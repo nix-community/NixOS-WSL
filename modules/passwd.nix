47be2f16e01a8f3dc771e320db9dab06310dbf51
nzbr <mail@nzbr.de>
2023-07-14 09:53:37 +0200

    fix tarball /etc/passwd

--- modules/passwd.nix
+++ modules/passwd.nix
@@ -2,7 +2,13 @@
 writeText "passwd" (
   lib.concatStringsSep "\n" (
     map
-      (user: "${user.name}:x:${toString user.uid}:${toString config.users.groups.${user.group}.gid}:${user.description}:${user.home}:${if shellOverride != null then shellOverride else toShellPath user.shell}}")
-      (lib.attrValues config.users.users)
+      (user: "${user.name}:x:${toString user.uid}:${toString config.users.groups.${user.group}.gid}:${user.description}:${user.home}:${if shellOverride != null then shellOverride else toShellPath user.shell}")
+      (lib.sort
+        (a: b: a.uid < b.uid)
+        (lib.filter
+          (u: u.uid != null)
+          (lib.attrValues config.users.users)
+        )
+      )
   )
 )
