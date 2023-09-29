9bff9a09a4eb89ca814d4e069c00e52f6c901cdf
K900 <me@0upti.me>
2023-09-29 22:31:16 +0300

    fix: remove deprecated options from test

--- tests/docker/docker-native.nix
+++ tests/docker/docker-native.nix
@@ -1,9 +1,5 @@
-{ pkgs, lib, ... }:
 {
   imports = [ ./base.nix ];
-
-  wsl.docker-native.enable = true;
-  wsl.docker-native.addToDockerGroup = true;
 
   # Github Actions runners try to use aufs and fail if this is not set explicitly
   virtualisation.docker.daemon.settings = {
