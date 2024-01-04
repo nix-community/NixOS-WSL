5786d3fbd96d3f8dcb00498072368711f1235307
Yang, Bo <yang-bo@yang-bo.com>
2024-01-04 18:29:46 +0000

    Hard-code file names when making symlink for drivers

--- modules/wsl-distro.nix
+++ modules/wsl-distro.nix
@@ -77,7 +77,24 @@ in
       extraPackages = mkIf cfg.useWindowsDriver [
         (pkgs.runCommand "wsl-lib" { } ''
           mkdir "$out"
-          ln -s /usr/lib/wsl/lib "$out/lib"
+          ln -s /usr/lib/wsl/lib/libcudadebugger.so.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libcuda.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libcuda.so.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libcuda.so.1.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libd3d12core.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libd3d12.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libdxcore.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvcuvid.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvcuvid.so.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvdxdlkernels.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvidia-encode.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvidia-encode.so.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvidia-ml.so.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvidia-opticalflow.so.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvoptix.so.1 "$out/lib"
+          ln -s /usr/lib/wsl/lib/libnvwgf2umx.so "$out/lib"
+          ln -s /usr/lib/wsl/lib/nvidia-smi "$out/lib"
         '')
       ];
     };
