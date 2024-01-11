4fb2b9f85c458a8885370bb679f185b6665e7adb
nzbr <mail@nzbr.de>
2024-01-11 18:04:20 +0100

    fix launcher nix build script

--- Launcher/default.nix
+++ Launcher/default.nix
@@ -1,7 +1,6 @@
 { pkgs ? import <nixpkgs> { }
 , stdenv ? pkgs.stdenv
 , lib ? pkgs.lib
-, writeText ? pkgs.writeText
 , dotnet-sdk ? pkgs.dotnet-sdk
 , dotnetPackages ? pkgs.dotnetPackages
 , fetchurl ? pkgs.fetchurl
@@ -9,10 +8,10 @@
 }:
 
 let
-  fetchNuGet = { pname, version, sha256 }: fetchurl {
+  fetchNuGet = { pname, version, hash }: fetchurl {
     name = "${pname}-${version}.nupkg";
     url = "https://www.nuget.org/api/v2/package/${pname}/${version}";
-    inherit sha256;
+    inherit hash;
   };
 in
 stdenv.mkDerivation rec {
@@ -26,10 +25,13 @@ stdenv.mkDerivation rec {
   ];
 
   nugetDeps = linkFarmFromDrvs "${name}-nuget-deps" (map fetchNuGet [
-    { pname = "Microsoft.AspNetCore.App.Runtime.win-x64"; version = "6.0.0"; sha256 = "dacDLzjHNLXHIWdq+cBbXOfPpiqgHsWOp8cwrk+yDMk="; }
-    { pname = "Microsoft.NETCore.App.Host.win-x64"; version = "6.0.0"; sha256 = "3c1kDGpCrwlX858lyoE+u0fgmRgRwN692KqRN0ejJoA="; }
-    { pname = "Microsoft.NETCore.App.Runtime.win-x64"; version = "6.0.0"; sha256 = "RpVKg/7oiOSbPBNFgn1kt/+a55GRqv5QXtAO/+K0oY8="; }
-    { pname = "System.CommandLine"; version = "2.0.0-beta3.22114.1"; sha256 = "lcqMVesbt2E189e6KfG8HVmJc3hjo2ht48WnH1UaMkY="; }
+    { pname = "Microsoft.AspNetCore.App.Runtime.win-x64"; version = "6.0.25"; hash = "sha256-njOrSWL+YJJvnywJ0clMzYdaANYl3rcVaZeZAki1erk="; }
+    { pname = "Microsoft.AspNetCore.App.Runtime.win-arm64"; version = "6.0.25"; hash = "sha256-Jnt9WIxGvvejtBW5mjWCXlYmsUj6fxVI4Xs3WWsr8YE="; }
+    { pname = "Microsoft.NETCore.App.Host.win-x64"; version = "6.0.25"; hash = "sha256-ATiO6e8xcDPrRngmT9k//7Z2Mx07hJhxgmdwUvMYJA0="; }
+    { pname = "Microsoft.NETCore.App.Host.win-arm64"; version = "6.0.25"; hash = "sha256-xvj8CZHY4PyXKroniWj0JIoksKf4pcbberfMf5XZYaE="; }
+    { pname = "Microsoft.NETCore.App.Runtime.win-x64"; version = "6.0.25"; hash = "sha256-Lk6cn0cSQVp4QVf6IznE7UnZ9tYxZmRMYrsTIHq9Vg8="; }
+    { pname = "Microsoft.NETCore.App.Runtime.win-arm64"; version = "6.0.25"; hash = "sha256-eCoXKCHkrteypFVOUctV3ReQk/lFjJ7cI/CVk2tNrZY="; }
+    { pname = "System.CommandLine"; version = "2.0.0-beta3.22114.1"; hash = "sha256-lcqMVesbt2E189e6KfG8HVmJc3hjo2ht48WnH1UaMkY="; }
   ]);
 
   unpackPhase = ''
@@ -45,12 +47,14 @@ stdenv.mkDerivation rec {
     export DOTNET_NOLOGO=1
     export DOTNET_CLI_TELEMETRY_OPTOUT=1
 
-    dotnet nuget disable source nuget.org
     nuget sources Add -Name nix -Source "$PWD/nix"
     nuget init "$nugetDeps" "$PWD/nix"
+    dotnet nuget disable source nuget.org
 
     mkdir -p $HOME/.nuget/NuGet
     cp $HOME/.config/NuGet/NuGet.Config $HOME/.nuget/NuGet
+
+    dotnet restore --ignore-failed-sources
 
     runHook postConfigure
   '';
@@ -60,15 +64,19 @@ stdenv.mkDerivation rec {
 
     mkdir -p $out
 
-    dotnet publish \
-      --self-contained \
-      -p:ContinuousIntegrationBuild=true \
-      -p:Deterministic=true \
-      --packages "$HOME/nuget_pkgs" \
-      -r win-x64 \
-      -c Release \
-      --output $out/bin \
-      Launcher
+    ${lib.concatStringsSep "\n" (
+      map (runtime: ''
+        dotnet publish \
+        --self-contained \
+        -p:ContinuousIntegrationBuild=true \
+        -p:Deterministic=true \
+        --packages "$HOME/nuget_pkgs" \
+        -r ${runtime} \
+        -c Release \
+        --output $out/${runtime} \
+        Launcher
+      '') [ "win-x64" "win-arm64" ]
+    )}
 
     runHook postBuild
   '';
