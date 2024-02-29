98afbe7407c0538d32d2f60ebf7ecd3fb6444f0c
nzbr <mail@nzbr.de>
2024-02-29 16:30:10 +0100

    remove --verbose flag

--- tests/lib/lib.ps1
+++ tests/lib/lib.ps1
@@ -42,7 +42,7 @@ class Distro {
     $LASTEXITCODE | Should -Be 0
 
     # Rebuild
-    $this.Launch("sudo nixos-rebuild switch --verbose < /dev/null")
+    $this.Launch("sudo nixos-rebuild switch < /dev/null")
     $LASTEXITCODE | Should -Be 0
 
     Write-Host "Config installed successfully"
