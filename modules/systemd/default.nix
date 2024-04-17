6c093d32c17d391761626171794a501352cbec85
Sandro Jäckel <sandro.jaeckel@gmail.com>
2024-04-17 10:56:28 +0200

    Cleanup, remove with lib

--- modules/systemd/default.nix
+++ modules/systemd/default.nix
@@ -1,42 +1,35 @@
 { config, lib, ... }:
-with lib; {
 
+{
   imports = [
     ./native
     ./syschdemd
   ];
 
-  options.wsl = with types; {
-    nativeSystemd = mkOption {
-      type = bool;
+  options.wsl = {
+    nativeSystemd = lib.mkOption {
+      type = lib.types.bool;
       default = true;
       description = "Use native WSL systemd support";
     };
   };
 
-  config =
-    let
-      cfg = config.wsl;
-    in
-    mkIf (cfg.enable) {
+  config = lib.mkIf config.wsl.enable {
+    # useful for usbip but adds a dependency on various firmwares which are combined over 300 MB big
+    services.udev.enable = lib.mkDefault false;
 
+    systemd = {
       # systemd-oomd requires cgroup pressure info which WSL doesn't have
-      systemd.oomd.enable = false;
-
-      # useful for usbip but adds a dependency on various firmwares which are combined over 300 MB big
-      services.udev.enable = lib.mkDefault false;
-
-      systemd = {
-        # Disable systemd units that don't make sense on WSL
-        services = {
-          firewall.enable = false;
-          # systemd-timesyncd actually works in WSL and without it the clock can drift
-          systemd-timesyncd.unitConfig.ConditionVirtualization = "";
-        };
-
-        # Don't allow emergency mode, because we don't have a console.
-        enableEmergencyMode = false;
+      oomd.enable = false;
+      # Disable systemd units that don't make sense on WSL
+      services = {
+        firewall.enable = false;
+        # systemd-timesyncd actually works in WSL and without it the clock can drift
+        systemd-timesyncd.unitConfig.ConditionVirtualization = "";
       };
 
+      # Don't allow emergency mode, because we don't have a console.
+      enableEmergencyMode = false;
     };
+  };
 }
