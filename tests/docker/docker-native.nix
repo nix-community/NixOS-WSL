9f050a46c89123cb1eceabcaea875e1ad3d3aaa6
nzbr <mail@nzbr.de>
2024-05-09 23:38:57 +0200

    test(docker): use default storage driver, remove -t, enable native systemd

--- tests/docker/docker-native.nix
+++ tests/docker/docker-native.nix
@@ -4,13 +4,8 @@
   ];
 
   wsl.enable = true;
-  wsl.nativeSystemd = false;
 
   users.users.nixos.extraGroups = [ "docker" ];
 
-  virtualisation.docker = {
-    enable = true;
-    # Github Actions runners try to use aufs and fail if this is not set explicitly
-    daemon.settings."storage-driver" = "vfs";
-  };
+  virtualisation.docker.enable = true;
 }
