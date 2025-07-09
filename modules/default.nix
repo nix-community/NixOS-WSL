464ae172d39b2c30bfa21d1cbbf5a60c2fe011d3
nzbr <mail@nzbr.de>
2025-07-09 03:10:56 +0200

    remove 24.05 compatibility code / documentation

--- modules/default.nix
+++ modules/default.nix
@@ -1,9 +1,5 @@
 { lib, ... }: {
   imports = [
-    # required for options from 24.11 to work on 24.05
-    # TODO: remove when 24.11 becomes stable
-    ./24.05-compat.nix
-
     ./build-tarball.nix
     ./docker-desktop.nix
     ./interop.nix
