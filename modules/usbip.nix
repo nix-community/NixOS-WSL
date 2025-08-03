257fefb7e79db8c1f7be17a80a0c5f2f8a662b18
nzbr <mail@nzbr.de>
2025-08-04 00:18:02 +0200

    Revert "use lib.getExe"
    This reverts commit a5caa989726f9576e9dc9829c62a267276f6b505.

--- modules/usbip.nix
+++ modules/usbip.nix
@@ -45,9 +45,9 @@ in
     services.udev.enable = true;
 
     wsl.extraBin = [
-      { src = lib.getExe pkgs.coreutils; }
-      { src = lib.getExe pkgs.coreutils; }
-      { src = lib.getExe pkgs.kmod; }
+      { src = "${pkgs.coreutils}/bin/cat"; }
+      { src = "${pkgs.coreutils}/bin/ls"; }
+      { src = "${pkgs.kmod}/bin/modprobe"; }
     ];
 
     systemd = {
