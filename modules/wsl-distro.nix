c9ba2fac1cd980018c53ded6d090b07f9b73361d
Arson Copperflame <git@nzbr.de>
2026-09-10 14:01:15 +0200

    Add /bin/true (#1103)

--- modules/wsl-distro.nix
+++ modules/wsl-distro.nix
@@ -209,6 +209,7 @@ in
         { src = "${cfg.binShExe}"; name = "sh"; }
         { src = "${pkgs.util-linux}/bin/mount"; }
         { src = "${pkgs.bashInteractive}/bin/bash"; }
+        { src = "${pkgs.coreutils}/bin/true"; }
       ];
     };
 
